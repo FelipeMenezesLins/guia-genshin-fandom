2332b385c65e2d27874855460a042a7f3ab1088d
Felipe Menezes Lins <felipemenezesmk22@gmail.com>
2024-06-30 14:07:40 -0300

    add

---
+++ pers-menu.js
@@ -0,0 +1,21 @@
+let star5 = document.getElementById('star-5')
+let star4 = document.getElementById('star-4')
+
+
+function cinco() {
+    if(star5.style.display == 'none'){
+        star5.style.display = 'block'
+        star4.style.display = 'none'
+    } else {
+        star4.style.display = 'none'
+    }
+}
+
+function quatro() {
+    if(star4.style.display == 'none'){
+        star4.style.display = 'block'
+        star5.style.display = 'none'
+    } else {
+        star5.style.display = 'none'
+    }
+}
