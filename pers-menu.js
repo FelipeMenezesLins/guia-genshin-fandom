169ebdec3e2d04f308deed0607265d88d4a79198
Felipe Menezes Lins <felipemenezesmk22@gmail.com>
2024-07-05 21:30:31 -0300

    add e att

--- pers-menu.js
+++ pers-menu.js
@@ -1,6 +1,6 @@
 let star5 = document.getElementById('star-5')
 let star4 = document.getElementById('star-4')
-let fogo = document.getElementsByName('pyro')
+
 
 function cinco() {
     if(star5.style.display == 'none'){
@@ -20,6 +20,48 @@ function quatro() {
     }
 }
 
-function pyro() {
-    
+function elementos(name) {
+    let div = document.querySelectorAll('.bloco')
+ 
+
+for(let i = 0; i<div.length; i++) {
+    let divs = div[i]
+    if(divs.classList.contains(name)) {
+         divs.style.display = 'block'
+    } else {
+        divs.style.display = 'none'
+    }
+} 
 }
+
+let pyro = document.getElementById('pyro').onclick = function() {
+    elementos('pyro')
+}
+
+let hydro = document.getElementById('hydro').onclick = function() {
+    elementos('hydro')
+}
+
+let geo = document.getElementById('geo').onclick = function() {
+    elementos('geo')
+}
+
+let anemo = document.getElementById('anemo').onclick = function() {
+    elementos('anemo')
+}
+
+let cyro = document.getElementById('cyro').onclick = function() {
+    elementos('cyro')
+}
+
+let electro = document.getElementById('electro').onclick = function() {
+    elementos('electro')
+}
+
+let dendro = document.getElementById('dendro').onclick = function() {
+    elementos('dendro')
+}
+
+
+
+
