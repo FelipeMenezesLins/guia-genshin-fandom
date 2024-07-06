4bdc22e78e8b914bf12aab20116c4c951a86ea62
Felipe Menezes Lins <felipemenezesmk22@gmail.com>
2024-07-06 20:48:33 -0300

    add

--- pers-menu.js
+++ pers-menu.js
@@ -4,7 +4,7 @@ let star4 = document.getElementById('star-4')
 
 function cinco() {
     if(star5.style.display == 'none'){
-        star5.style.display = 'block'
+        star5.style.display = 'flex'
         star4.style.display = 'none'
     } else {
         star4.style.display = 'none'
@@ -13,7 +13,7 @@ function cinco() {
 
 function quatro() {
     if(star4.style.display == 'none'){
-        star4.style.display = 'block'
+        star4.style.display = 'flex'
         star5.style.display = 'none'
     } else {
         star5.style.display = 'none'
@@ -60,6 +60,36 @@ let electro = document.getElementById('electro').onclick = function() {
 
 let dendro = document.getElementById('dendro').onclick = function() {
     elementos('dendro')
+}
+
+function armamento(armas) {
+    let div = document.querySelectorAll('.bloco')
+ 
+
+for(let i = 0; i<div.length; i++) {
+    let divs = div[i]
+    if(divs.classList.contains(armas)) {
+         divs.style.display = 'block'
+    } else {
+        divs.style.display = 'none'
+    }
+} 
+}
+
+let espada = document.getElementById('espada').onclick = function () {
+    armamento('espada')
+}
+let espadao = document.getElementById('espadao').onclick = function () {
+    armamento('espadao')
+}
+let arco = document.getElementById('arco').onclick = function () {
+    armamento('arco')
+}
+let lança = document.getElementById('lança').onclick = function () {
+    armamento('lança')
+}
+let livro = document.getElementById('livro').onclick = function () {
+    armamento('livro')
 }
 
 
